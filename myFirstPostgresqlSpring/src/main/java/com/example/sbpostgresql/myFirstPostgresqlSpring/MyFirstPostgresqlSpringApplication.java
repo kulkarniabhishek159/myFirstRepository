c52e76fd73501282e04681eaf5343552671cf304
HP <HP@ABHISHEK>
2020-04-22 12:46:00 +0530

    First Commit

---
+++ myFirstPostgresqlSpring/src/main/java/com/example/sbpostgresql/myFirstPostgresqlSpring/MyFirstPostgresqlSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbpostgresql.myFirstPostgresqlSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstPostgresqlSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstPostgresqlSpringApplication.class, args);
+	}
+
+}
---
+++ myFirstPostgresqlSpring/src/test/java/com/example/sbpostgresql/myFirstPostgresqlSpring/MyFirstPostgresqlSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbpostgresql.myFirstPostgresqlSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstPostgresqlSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
